ae2ab6ae39b8840dd552748ef6a5ba04a98c29a3
Ericdid <ericdidom@gmail.com>
2023-09-06 21:31:34 +0200

    add.structure !!BUGGED!!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
+// axios
+//   .get("https://flynn.boolean.careers/exercises/api/random/mail")
+//   .then((response) => {
+//     console.log(response.data.response);
+//   });
+
 const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      email: "",
+    };
+  },
+  methods: {
+    generateEmail() {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            let randomEmail = response.data.response;
+            this.email = randomEmail;
+            console.log(response.data);
+          });
+      }
+    },
   },
 }).mount("#app");
